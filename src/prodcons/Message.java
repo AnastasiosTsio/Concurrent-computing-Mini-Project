925ef11291538366507270c0823db6d258ed4059
noahkohrs <noah@kohrs.fr>
2023-12-05 15:57:00 +0100

    Remove unused code in Message class

--- src/prodcons/Message.java
+++ src/prodcons/Message.java
@@ -3,22 +3,11 @@ package prodcons;
 
 public class Message{
     Object msg ;
-    Object creator;
 
     public Message(Object msg) {
         this.msg = msg;
     }
-    public Message(Object msg, Object sender) {
-        this.msg = msg;
-        this.creator = sender;
-    }
     public Object getMsg() {
         return msg;
     }
-    public Object getCreator() {
-        return creator;
-    }
-    // public String toString() {
-    //     return msg.toString();
-    // }
 }
